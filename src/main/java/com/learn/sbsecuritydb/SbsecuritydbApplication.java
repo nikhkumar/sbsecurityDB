ad1b98e491d2b08e59228c42e09198460ac61240
NIKHILKUMAR <NIKHILKUMAR@nikhkumar@gmail.com>
2019-05-14 01:01:02 +0530

    Initial Commit

---
+++ src/main/java/com/learn/sbsecuritydb/SbsecuritydbApplication.java
@@ -0,0 +1,13 @@
+package com.learn.sbsecuritydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsecuritydbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbsecuritydbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/sbsecuritydb/SbsecuritydbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.sbsecuritydb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbsecuritydbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
